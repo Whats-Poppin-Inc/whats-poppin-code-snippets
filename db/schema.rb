2b0ae974729e033315f3325b0858d0998a40b216
Miles Weber <milesweber@comcast.net>
2022-07-20 10:25:49 -0400

    Added Posts Model/Views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_134848) do
+ActiveRecord::Schema.define(version: 2022_07_20_142441) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "lang"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
